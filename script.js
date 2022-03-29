49380bc910010348f40b30528e6730be1e7aafa3
Giuseppe <giuseppeuhlmann@gmail.com>
2022-03-29 18:18:35 -0300

    feat: adicionando estilo

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
+  const div = document.createElement('div');
   img.className = 'item__image';
   img.src = imageSource;
-  return img;
+  div.className = 'thumb';
+  div.appendChild(img);
+  return div;
 }
 
 function createCustomElement(element, className, innerText) {
@@ -78,12 +81,13 @@ const addToCart = async (event) => {
   addItemLocalStorage(obj);
 };
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ sku, name, price, image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('span', 'item__price', price));
   section.appendChild(createCustomElement('span', 'item__title', name));
   const btn = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
   btn.onclick = addToCart;
@@ -105,19 +109,25 @@ const hideLoad = () => {
   load.remove();
 };
 
-const showcase = async (product) => {
-  showLoad();
-
-  const obj = await fetchProducts(product);
-  
-  await hideLoad();
+const productsObj = (obj) => {
   const item = obj.results.map((elem) => (
     {
       sku: elem.id,
-      name: elem.title,
+      name: `${elem.title.length > 50 ? `${elem.title.substring(0, 50)}...` : elem.title}`,
+      price: `R$ ${elem.price % 1 === 0 ? `${elem.price}.00` : elem.price}`,
       image: `https://http2.mlstatic.com/D_${elem.thumbnail_id}-O.jpg`,
     }
   ));
+
+  return item;
+};
+
+const showcase = async (product) => {
+  showLoad();
+  const obj = await fetchProducts(product);
+  hideLoad();
+
+  const item = productsObj(obj);
 
   item.forEach((elem) => {
     const element = document.querySelector('.items');
@@ -143,8 +153,6 @@ const showCart = () => {
 const slideCart = () => {
   const cart = document.querySelector('.cart');
   cart.classList.toggle('active');
-  const cartTitle = document.querySelector('.container-cartTitle');
-  cartTitle.classList.toggle('active');
 };
 
 const searchProduct = () => {
